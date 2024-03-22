b5aaec7108a40bbbed40b43090236b160dd27f4b
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-03-22 19:47:14 +0600

    * New File Added *

--- Doubly Linked List.py
+++ Doubly Linked List.py
@@ -24,6 +24,69 @@ class DoublyList:
             temp = temp.next
         return count
 
+    def nodeAt(self, index):
+        total_nodes = self.countNode()
+        head = self.head
+        temp = head.next
+        # CHECKING IF THE GIVEN 'index' IS VALID OR NOT!
+        if index >= total_nodes or index < 0:
+            print("Index Out of Range!!!")
+
+        if index == total_nodes:
+            return head.prev
+        elif index == 0:
+            return temp
+        else:
+            loop_counter = 0
+            while temp != head:
+                if loop_counter == index:
+                    return temp
+                temp = temp.next
+                loop_counter += 1
+
+    def insert(self, elem, index):
+        head = self.head
+        tail = head.next
+        if index >= self.countNode() + 1 or index < 0:
+            print("Index Out of Range!!!")
+
+        my_new_node = Node(elem, None, None)  # Creating a new "Node" class
+
+        loop_counter = 0
+        while tail != head:
+            if loop_counter == index:
+                temp = tail.prev
+                my_new_node.next = tail
+                my_new_node.prev = temp
+                temp.next = my_new_node
+                tail.prev = my_new_node
+            tail = tail.next
+            loop_counter += 1
+
+    def remove(self, index):
+        head = self.head
+        temp = head.next
+        total_nodes = self.countNode()
+        if index >= total_nodes or index < 0:
+            print("Index Out of Range!!!")
+
+        loop_counter = 0
+        while temp != head:
+            # The code lines inside if will only run Once when it finds the right NODE to remove
+            if loop_counter == index:
+                previus_node = temp.prev
+                next_node = temp.next
+                previus_node.next = next_node
+                next_node.prev = previus_node
+                removed = temp.element
+                temp.prev = None
+                temp.next = None
+                return str(removed)
+
+            temp = temp.next
+            loop_counter += 1
+
+
     def forwardPrint(self):
         pointer = self.head.next
         while pointer != self.head:
@@ -42,36 +105,23 @@ class DoublyList:
                 print(pointer.element)
             pointer = pointer.prev
 
-    def insert(self, elem, idx):
-        head = self.head
-        tail = head.next
 
-        if idx >= self.countNode() + 1 or idx < 0:
-            return None
 
-        myNewNode = Node(elem, None, None)
 
-        loop_counter = 0
-        while tail != head:
-            if loop_counter == idx:
-
-                pred = tail.prev
-
-                myNewNode.next = tail
-                myNewNode.prev = pred
-
-                pred.next = myNewNode
-                tail.prev = myNewNode
-
-            tail = tail.next
-            loop_counter += 1
-
-print()
-##########
-# >>> Task-5 <<<
-print(">>> Task-5<<<")
+print("####################################################")
 my_arr = [10,34,12,4,-99,100]
 myDoublyLL = DoublyList(my_arr)
 print(myDoublyLL.head.prev.element)
+print("#####################################################")
 myDoublyLL.forwardPrint()
 myDoublyLL.backwardPrint()
+print("#####################################################")
+myDoublyLL.insert(999999,3)
+myDoublyLL.forwardPrint()
+print("#####################################################")
+myNode = myDoublyLL.nodeAt(3)
+print(myNode.element)
+print("#####################################################")
+removedNode = myDoublyLL.nodeAt(3)
+myDoublyLL.forwardPrint()
+print("Node Removed : "+str(removedNode.element))
